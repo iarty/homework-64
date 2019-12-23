83a97928877d0aa4cb05dfcce1ffc23efe3abd05
Barbonov Artem <abarbonov@gmail.com>
2019-12-23 21:17:09 +0600

    completed project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import ToDo from "./containers/ToDo/ToDo";
+import Films from "./containers/Films/Films";
+import PersonalNotes from "./containers/PersonalNotes/Home/Home";
+import NotesForm from "./containers/PersonalNotes/NotesForm/NotesForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Fragment>
+			<Navigation />
+			<Switch>
+				<Route path='/films' component={Films} />
+				<Route path='/notes/add' component={NotesForm} />
+				<Route path='/notes/:id' render={(props) => <NotesForm {...props} btn="Edit" headline="Edit note" />} />
+				<Route path='/notes' component={PersonalNotes} />
+				<Route path={"/" || "/todo"} component={ToDo} />
+			</Switch>
+		</Fragment>
+	);
 }
-
-export default App;
